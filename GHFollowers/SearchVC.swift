febadc0f923741c902e630ac2d7a6799d970339e
Weerawut Chaiyasomboon <weerawut@live.com>
2024-01-07 14:13:10 +0700

    Create GFTitleLabel, GFBodyLabel, GFAlertVC. Do dismiss and present VC modally.

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -80,7 +80,11 @@ class SearchVC: UIViewController {
     
     @objc func pushFollowerListVC(){
         guard isUsernameEntered else {
-            print("No username!")
+            presentGFAlertOnMainThread(
+                title: "Empty Username",
+                message: "Please enter a username. We need to know who to look for 😎.",
+                buttonTitle: "Ok"
+            )
             return
         }
         
