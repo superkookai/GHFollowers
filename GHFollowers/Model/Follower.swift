b08db8c945da26c1c2256bbe6983fbe843a9389f
Weerawut Chaiyasomboon <weerawut@live.com>
2024-01-09 10:17:33 +0700

    Add Model >> User and Follower.

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Weerawut Chaiyasomboon on 9/1/2567 BE.
+//
+
+import Foundation
+
+struct Follower: Codable{
+    var login: String
+    var avatarUrl: String
+    
+}
